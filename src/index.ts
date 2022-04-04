79a544577c536999c22ceba889270e3517c5f10b
odm275 <pomejia@gmail.com>
2022-04-04 10:07:43 -0500

    changed set up

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const PORT = 5000;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+    console.log('This endpoint was hit');
+    res.send('Hit that');
+});
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
